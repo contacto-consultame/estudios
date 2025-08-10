7007ba73d18c911606dd3305027f69e360d4c6ee
Luis <contacto@solucionpro.cl>
2025-08-10 12:13:02 -0400

    otro commit

--- saludos.sh
+++ saludos.sh
@@ -1,4 +1,4 @@
 echo "Hola mundo con jenkins y github"
 echo "Hola mundo con jenkins y github"
-echo "Prueba que ya conecta con github2
-"
+echo "Prueba que ya conecta con github2"
+echo "probando trigger con webhook desde git y usando grok"
