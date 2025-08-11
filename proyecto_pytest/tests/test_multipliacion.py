a31bed2e6980d121808e46b821c07efbae1e7550
Luis <contacto@solucionpro.cl>
2025-08-11 03:52:19 -0400

    proyecto python

---
+++ proyecto_pytest/tests/test_multipliacion.py
@@ -0,0 +1,12 @@
+import pytest
+
+def multiplicar(a, b):
+
+    """Funcion de multiplicar de dos numeros"""
+
+    return a*b
+
+def test_multiplicar():
+    assert multiplicar(1, 2) == 2
+    assert multiplicar(-1, 1) == -1
+    assert multiplicar(0, 100) == 0
